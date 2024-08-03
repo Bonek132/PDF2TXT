fdfa2266bca121757d505d22c079f55dbbd311d0
Dawid <dawidbonkowski26@gmail.com>
2024-08-03 14:05:20 +0200

    added drop feature

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def open_file():
     filepath = filedialog.askopenfilename(filetypes=[("PDF files","*.pdf"), ("All Files"),("*.*")])
     if filepath:
         selected_file.set(filepath)
+        
+def drop(event):
+    file_patch = event.data
+    if file_patch.starwith("{") and file_patch.endswith("}"):
+        file_patch = file_patch[1:-1]
+    selected_file.set(file_patch)
 
 def choose_directory():
     pass
@@ -21,6 +27,9 @@ root = TkinterDnD.Tk()
 root.title("PDF converter")
 root.geometry("500x270")
 
+root.drop_target_register(DND_FILES)
+root.dnd_bind("<<Drop>>", drop)
+
 output_dir = tk.StringVar
 output_file = tk.StringVar
 selected_file = tk.StringVar
@@ -32,7 +41,7 @@ tk.Button(root, text="Browse", command=choose_directory).pack()
 tk.Label(root, text="Output file name").pack()
 tk.Entry(root, textvariable=output_file).pack()
 
-tk.Label(root, text="Select PDF file").pack()
+tk.Label(root, text="Select or drop PDF file").pack()
 tk.Entry(root, textvariable=selected_file, state="readonly").pack()
 
 open_button = tk.Button(root, text="Open PDF", command=open_file)
